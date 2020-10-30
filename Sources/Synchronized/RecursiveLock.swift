e8bbab660d61b4f78fd94c3640f1a03886c326ea
Anthony Drendel <atdrendel@users.noreply.github.com>
2020-10-30 22:25:10 +0100

    Version 2.1.0 (#5)

--- Sources/Synchronized/RecursiveLock.swift
+++ Sources/Synchronized/RecursiveLock.swift
@@ -16,15 +16,21 @@ final public class RecursiveLock {
     }
 
     public func locked<T>(_ block: () throws -> T) rethrows -> T {
-        pthread_mutex_lock(&_backing)
+        let ret = pthread_mutex_lock(&_backing)
+        // https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/pthread_mutex_lock.3.html
+        precondition(ret == 0, "Could not acquire lock: '\(ret)'")
         defer { pthread_mutex_unlock(&_backing) }
         return try block()
     }
 
-    public func tryLocked(_ block: () throws -> Void) rethrows {
+    public func tryLocked(_ block: () throws -> Void) rethrows -> Bool {
+        // https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/pthread_mutex_trylock.3.html#//apple_ref/doc/man/3/pthread_mutex_trylock
         if pthread_mutex_trylock(&_backing) == 0 {
             defer { pthread_mutex_unlock(&_backing) }
             try block()
+            return true
+        } else {
+            return false
         }
     }
 }
